7afb843265a951f08fb22618c4886499a8f10cfd
Park-chan-young <k30538404@gmail.com>
2023-04-11 16:13:04 +0900

    Initial Commit

---
+++ N02/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  N02
+//
+//  Created by 박찬영 on 2023/04/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
